071a0c92d377f5910c89559ff9217fa49397bd97
Julian Koripamo <jkoripamo@gmail.com>
2022-07-20 17:36:40 +0100

    creating a TypeAdapter

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,5 +1,13 @@
+import 'package:hive/hive.dart';
+
+part 'contact.g.dart';
+
+@HiveType(typeId: 0)
 class Contact {
+  @HiveField(0)
   final String name;
+
+  @HiveField(1)
   final int phone;
 
   Contact(this.name, this.phone);
